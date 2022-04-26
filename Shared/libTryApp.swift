b21c321f4ac632147b3263d61f2488150ece1927
Shota Shimazu <hornet.live.mf@gmail.com>
2022-04-26 19:30:39 +0900

    add new SwiftUI proj

---
+++ Shared/libTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  libTryApp.swift
+//  Shared
+//
+//  Created by Shota Shimazu on 2022/04/26.
+//
+
+import SwiftUI
+
+@main
+struct libTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
