8bd31d5f75461d9ad2e4fdc61ae269df1b22e74d
Shota Shimazu <hornet.live.mf@gmail.com>
2022-07-18 00:51:13 +0900

    revise

---
+++ Sources/libTry/CommandSet.swift
@@ -0,0 +1,16 @@
+//
+//  CommandSet.swift
+//  
+//
+//  Created by Shota Shimazu on 2022/07/18.
+//
+
+import Foundation
+
+
+class CommandSet {
+
+    let buildSet = [
+        "xcrun build etc...",
+    ]
+}
